d0ef8c657970e2ee8db84b4297ddf19a3f472705
vishalparmar19422 <parmarvishal1942@gmail.com>
2025-02-21 15:35:53 +0530

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+import { minimalSetup, EditorView } from "codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+
+function App() {
+  return (
+    <>
+     <h1 className="text-green-400">Hello </h1>
+    </>
+  );
+}
+
+export default App;
